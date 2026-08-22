30e410fd2bcaa3928235bc7d0240db32d3e37c5d
muzammil-360 <shaikhmuzammil5361@gmail.com>
2026-08-22 21:15:29 +0530

    Time: 0 ms (100%), Space: 48.4 MB (14.2%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,41 +1,91 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {   
-        int f=first(nums,target);
-        if(f==nums.length || nums[f]!=target){return new int[]{-1,-1};}
-        int l=last(nums,target); 
-        return new int[]{f,l-1};
-    }
-    int first(int[] nums,int target){
-       int low=0;
-       int high=nums.length-1;
-       int ans=nums.length;
-       while(low<=high){
-        int mid=low+(high-low)/2;
-        if(nums[mid]>=target){
-            ans=mid;
-            high=mid-1;
-        }
-        else{
-            low=mid+1;
-        }
-       }
-       return ans;
+        // int f=first(nums,target);
+        // if(f==nums.length || nums[f]!=target){return new int[]{-1,-1};}
+        // int l=last(nums,target); 
+        // return new int[]{f,l-1};
+
+    int f=first(nums,target);
+    if(f==-1){return new int[]{-1,-1};}
+    int l=last(nums,target);
+    return new int[]{f,l};
+
+
+
     }
 
-       int last(int[] nums,int target){
-       int low=0;
-       int high=nums.length-1;
-       int ans=nums.length;
-       while(low<=high){
-        int mid=low+(high-low)/2;
-        if(nums[mid]>target){
-            ans=mid;
-            high=mid-1;
+        int first(int[] nums,int target){
+        int first=-1;
+        int low=0;
+        int high=nums.length-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                first=mid;
+                high=mid-1;
+            }
+            else if(nums[mid]>target){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
         }
-        else{
-            low=mid+1;
-        }
-       }
-       return ans;
+        return first;
     }
-}
+
+        int last(int[] nums,int target){
+        int last=-1;
+        int low=0;
+        int high=nums.length-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                last=mid;
+                low=mid+1;
+            }
+            else if(nums[mid]>target){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return last;
+    }
+
+
+    // int first(int[] nums,int target){
+    //    int low=0;
+    //    int high=nums.length-1;
+    //    int ans=nums.length;
+    //    while(low<=high){
+    //     int mid=low+(high-low)/2;
+    //     if(nums[mid]>=target){
+    //         ans=mid;
+    //         high=mid-1;
+    //     }
+    //     else{
+    //         low=mid+1;
+    //     }
+    //    }
+    //    return ans;
+    // }
+
+    //    int last(int[] nums,int target){
+    //    int low=0;
+    //    int high=nums.length-1;
+    //    int ans=nums.length;
+    //    while(low<=high){
+    //     int mid=low+(high-low)/2;
+    //     if(nums[mid]>target){
+    //         ans=mid;
+    //         high=mid-1;
+    //     }
+    //     else{
+    //         low=mid+1;
+    //     }
+    //    }
+    //    return ans;
+    // }
+ }
