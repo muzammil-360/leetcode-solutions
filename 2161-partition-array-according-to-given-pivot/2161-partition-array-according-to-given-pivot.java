3bcbf53b1f3fdfa9a5d687f594ea63a8afbf6d44
muzammil-360 <shaikhmuzammil5361@gmail.com>
2026-08-25 23:11:50 +0530

    Time: 5 ms (70.35%), Space: 159.9 MB (64.63%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
@@ -1,24 +1,37 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        int cntp=0;
-        List<Integer> ps=new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]<pivot)ps.add(nums[i]);
-            else if(nums[i]==pivot)cntp++;
-        }
-        List<Integer> pg=new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]>pivot)pg.add(nums[i]);
-        }   
+        // int cntp=0;
+        // List<Integer> ps=new ArrayList<>();
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]<pivot)ps.add(nums[i]);
+        //     else if(nums[i]==pivot)cntp++;
+        // }
+        // List<Integer> pg=new ArrayList<>();
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]>pivot)pg.add(nums[i]);
+        // }   
+        // int[] ans=new int[nums.length];
+        // for(int i=0;i<ps.size();i++){
+        //     ans[i]=ps.get(i);
+        // }
+        // for(int i=ps.size();i<(ps.size()+cntp);i++){
+        //     ans[i]=pivot;
+        // }
+        // for(int i=ps.size()+cntp;i<(ps.size()+cntp+pg.size());i++){
+        //     ans[i]=pg.get(i-(ps.size()+cntp));
+        // }
+        // return ans;
+
         int[] ans=new int[nums.length];
-        for(int i=0;i<ps.size();i++){
-            ans[i]=ps.get(i);
+        int ind=0;
+        for(int num:nums){
+            if(num<pivot)ans[ind++]=num;
         }
-        for(int i=ps.size();i<(ps.size()+cntp);i++){
-            ans[i]=pivot;
+        for(int num:nums){
+            if(num==pivot)ans[ind++]=num;
         }
-        for(int i=ps.size()+cntp;i<(ps.size()+cntp+pg.size());i++){
-            ans[i]=pg.get(i-(ps.size()+cntp));
+        for(int num:nums){
+            if(num>pivot)ans[ind++]=num;
         }
         return ans;
     }
