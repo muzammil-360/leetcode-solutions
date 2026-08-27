a7b1285e32995bae3a3ea114b4175872a73b568f
muzammil-360 <shaikhmuzammil5361@gmail.com>
2026-08-27 21:07:44 +0530

    Time: 26 ms (14.4%), Space: 47.9 MB (58.94%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -12,19 +12,20 @@ class Solution {
         // }
         int low=1;
         int high=max;
-        int ans=-1;
+        //int ans=-1;
         while(low<=high){
             int mid=low+(high-low)/2;
             int hr=cnthr(piles,mid);
             if(hr<=h){
-                ans=mid;
+                //ans=mid;
                 high=mid-1;
             }
             else{
                 low=mid+1;
             }
         }
-        return ans;
+        //return ans;
+        return low;
     }
     int cnthr(int[] piles,int hour){
         int counthr=0;
