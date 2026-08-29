5093dfeb3a2ac4fc0134980e3bf30762566728b0
muzammil-360 <shaikhmuzammil5361@gmail.com>
2026-08-29 23:22:21 +0530

    Time: 4 ms (35.77%), Space: 43.1 MB (56.42%) - LeetHub

--- 2465-number-of-distinct-averages/2465-number-of-distinct-averages.java
+++ 2465-number-of-distinct-averages/2465-number-of-distinct-averages.java
@@ -1,12 +1,12 @@
 class Solution {
     public int distinctAverages(int[] nums) {
         Arrays.sort(nums);
-        HashSet<Double> st=new HashSet<>();
+        HashSet<Integer> st=new HashSet<>();
         int l=0;
         int r=nums.length-1;
         while(l<r){
-            double avg=((double)nums[l]+(double)nums[r])/2.0;
-            st.add(avg);
+           // double avg=((double)nums[l]+(double)nums[r])/2.0;
+            st.add(nums[l]+nums[r]);
             l++;
             r--;
         }
