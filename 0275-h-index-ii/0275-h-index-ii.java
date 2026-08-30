8ecb972f5115946c83a55945cc6fdddc0dc81678
muzammil-360 <shaikhmuzammil5361@gmail.com>
2026-08-30 15:47:58 +0530

    Time: 3 ms (9.13%), Space: 50 MB (93.76%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int hIndex(int[] citations) {
+               int max=Integer.MIN_VALUE;
+        for(int i=0;i<citations.length;i++){
+            max=Math.max(max,citations[i]);
+        }
+        int low=1;
+        int high=max;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            boolean h=maxh(citations,mid);
+            if(h==true)low=mid+1;
+            else{high=mid-1;}
+        }
+        return high;
+    }
+    boolean maxh(int[] citations,int k){
+        int cit=0;
+        for(int i=0;i<citations.length;i++){
+            if(citations[i]>=k){
+                cit++;
+            }
+        }
+        if(cit>=k)return true;
+        return false;
+    }
+}
