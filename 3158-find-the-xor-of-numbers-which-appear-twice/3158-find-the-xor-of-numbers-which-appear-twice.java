dbd3b9f9af7209a143f85326668f7d4d013dafab
muzammil-360 <shaikhmuzammil5361@gmail.com>
2026-09-02 14:32:45 +0530

    Time: 3 ms (36.87%), Space: 44.4 MB (89.94%) - LeetHub

--- 3158-find-the-xor-of-numbers-which-appear-twice/3158-find-the-xor-of-numbers-which-appear-twice.java
+++ 3158-find-the-xor-of-numbers-which-appear-twice/3158-find-the-xor-of-numbers-which-appear-twice.java
@@ -1,18 +1,18 @@
 class Solution {
     public int duplicateNumbersXOR(int[] nums) {
-    //    HashSet<Integer> ans=new HashSet<>();
-    //     for(int i=0;i<nums.length;i++){
-    //         int cnt=0;
-    //         for(int j=0;j<nums.length;j++){
-    //             if(nums[i]==nums[j])cnt++;
-    //         }
-    //         if(cnt==2){ans.add(nums[i]);}
-    //     }
-    //     int xor=0;
-    //     for(int num:ans){
-    //         xor^=num;
-    //     }
-    //     return xor;
+       HashSet<Integer> ans=new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            int cnt=0;
+            for(int j=0;j<nums.length;j++){
+                if(nums[i]==nums[j])cnt++;
+            }
+            if(cnt==2){ans.add(nums[i]);}
+        }
+        int xor=0;
+        for(int num:ans){
+            xor^=num;
+        }
+        return xor;
 
     //   HashSet<Integer> seen = new HashSet<>();
     //     int xor = 0;
@@ -26,15 +26,15 @@ class Solution {
     //     }
     //     return xor;
 
-     Arrays.sort(nums);
+        // Arrays.sort(nums);
 
-        int xor = 0;
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == nums[i - 1]) {
-                xor ^= nums[i];
-            }
-        }
-        return xor;
+        // int xor = 0;
+        // for (int i = 1; i < nums.length; i++) {
+        //     if (nums[i] == nums[i - 1]) {
+        //         xor ^= nums[i];
+        //     }
+        // }
+        // return xor;
 
     }
 }
