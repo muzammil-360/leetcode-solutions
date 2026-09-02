64be6e10f8851d98cdbfd6ad7305531576dd8eee
muzammil-360 <shaikhmuzammil5361@gmail.com>
2026-09-02 23:09:56 +0530

    Time: 1 ms (77.53%), Space: 42.4 MB (12.2%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.java
+++ 1323-maximum-69-number/1323-maximum-69-number.java
@@ -1,26 +1,37 @@
 class Solution {
     public int maximum69Number (int num) {
-       List<Integer> ls=new ArrayList<>();
-        while(num>0){
-            int ld=num%10;
-            ls.add(ld);
-            num/=10;
-        }
-        Collections.reverse(ls);
-        int[] arr=new int[ls.size()];
-        for(int i=0;i<arr.length;i++){
-            arr[i]=ls.get(i);
-        }
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]==6){
-                arr[i]=9;
+    //    List<Integer> ls=new ArrayList<>();
+    //     while(num>0){
+    //         int ld=num%10;
+    //         ls.add(ld);
+    //         num/=10;
+    //     }
+    //     Collections.reverse(ls);
+    //     int[] arr=new int[ls.size()];
+    //     for(int i=0;i<arr.length;i++){
+    //         arr[i]=ls.get(i);
+    //     }
+    //     for(int i=0;i<arr.length;i++){
+    //         if(arr[i]==6){
+    //             arr[i]=9;
+    //             break;
+    //         }
+    //     }
+    //     int sol=0;
+    //     for(int i=0;i<arr.length;i++){
+    //         sol=sol*10+arr[i];
+    //     }
+    //     return sol;
+     char[] arr = String.valueOf(num).toCharArray();
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] == '6') {
+                arr[i] = '9';
                 break;
             }
         }
-        int sol=0;
-        for(int i=0;i<arr.length;i++){
-            sol=sol*10+arr[i];
-        }
-        return sol;
+
+        return Integer.parseInt(new String(arr));
     }
 }
