0faf381bbf72101db81d2d5e83bb380d8aae145a
muzammil-360 <shaikhmuzammil5361@gmail.com>
2026-09-03 14:10:06 +0530

    Time: 29 ms (15.94%), Space: 121.3 MB (84.78%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        Arrays.sort(nums1);
+        for(int i=1;i<nums1.length;i++){
+            if(nums1[i-1]%2==1 && nums1[i]%2==0){
+                if(nums1[i]-nums1[i-1]>=1)return true;
+            }
+            //else{return false;}
+            else if(nums1[i-1]%2==0 && nums1[i]%2==1){
+                if((nums1[i]-nums1[i-1])%2==1)return false;;
+                //return false;
+            }
+        }
+        return true;
+    }
+}
