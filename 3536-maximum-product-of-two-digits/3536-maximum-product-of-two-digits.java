67065279358546fdf7386cb23637ba813d677541
muzammil-360 <shaikhmuzammil5361@gmail.com>
2026-09-05 15:13:55 +0530

    Time: 1 ms (100%), Space: 42.8 MB (19%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
@@ -5,14 +5,14 @@ class Solution {
         int sm=-1;
         while(temp>0){
             int ld=temp%10;
-            if(ld>m){
+            if(ld>=m){
                 sm=m;
                 m=ld;
             }
             else if(ld<m && ld>sm){
                 sm=ld;
             }
-            else if(ld==m){sm=ld;}
+           // else if(ld==m){sm=ld;}
             temp/=10;
         }
         return m*sm;
