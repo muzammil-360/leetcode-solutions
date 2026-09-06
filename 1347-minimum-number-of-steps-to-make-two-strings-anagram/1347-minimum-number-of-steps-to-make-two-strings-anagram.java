184a3a5e363e5f47855eea525e68816ecfbc3f23
muzammil-360 <shaikhmuzammil5361@gmail.com>
2026-09-06 19:40:30 +0530

    Time: 52 ms (17.4%), Space: 47.6 MB (62%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.java
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.java
@@ -11,16 +11,25 @@ class Solution {
             tmap.put(ch,tmap.getOrDefault(ch,0)+1);
         }
         int cnt=0;
-        for (char ch : smap.keySet()){
-            if(!tmap.containsKey(ch)){
-                cnt+=smap.get(ch);
-            }
-            else{
-                int diff=smap.get(ch)-tmap.get(ch);
-                if(diff>=0){cnt+=diff;}
-                else{cnt+=0;}
-            }
-        }
+        // for (char ch : smap.keySet()){
+        //     if(!tmap.containsKey(ch)){
+        //         cnt+=smap.get(ch);
+        //     }
+        //     else{
+        //         int diff=smap.get(ch)-tmap.get(ch);
+        //         if(diff>=0){cnt+=diff;}
+        //         else{cnt+=0;}
+        //     }
+        // }
+        for(char ch : smap.keySet()){
+
+    int sFreq = smap.get(ch);
+    int tFreq = tmap.getOrDefault(ch, 0);
+
+    if(sFreq > tFreq){
+        cnt += sFreq - tFreq;
+    }
+}
         return cnt;
     }
 }
