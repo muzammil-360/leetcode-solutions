a93a57326feac345271b8ab6181bb78172efbc9a
muzammil-360 <shaikhmuzammil5361@gmail.com>
2026-09-08 13:58:29 +0530

    Time: 0 ms (100%), Space: 46.4 MB (80.24%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -1,20 +1,24 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
         int[] ans=new int[2*n];
-        int k=0;
-        int l=1;
-        int i=0;
-        while(k<2*n || l<2*n){
-           if(i<n){
-             ans[k]=nums[i];
-            i++;
-            k+=2;
-            }
-            else if(i<2*n){
-                ans[l]=nums[i];
-                i++;
-                l+=2;
-            }
+        // int k=0;
+        // int l=1;
+        // int i=0;
+        // while(k<2*n || l<2*n){
+        //    if(i<n){
+        //      ans[k]=nums[i];
+        //     i++;
+        //     k+=2;
+        //     }
+        //     else if(i<2*n){
+        //         ans[l]=nums[i];
+        //         i++;
+        //         l+=2;
+        //     }
+        // }
+        for(int i=0;i<nums.length/2;i++){
+            ans[2*i]=nums[i];
+            ans[2*i+1]=nums[i+n];
         }
         return ans;
     }
