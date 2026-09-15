e3c4f386a774b5f637a42426fa63359513f39f3d
muzammil-360 <shaikhmuzammil5361@gmail.com>
2026-09-15 14:26:45 +0530

    Time: 1 ms (88.31%), Space: 43.3 MB (25.17%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.java
+++ 0520-detect-capital/0520-detect-capital.java
@@ -5,8 +5,11 @@ class Solution {
         for(int ch=0;ch<word.length();ch++){
             char c=word.charAt(ch);
             int num=c-'0';
-             if(ch==0 && num<45){f=1;}
-            if(num<45)cnt++;
+            // if(ch==0 && num<45){f=1;}
+            // if(num<45)cnt++;
+            if(ch==0 && c>='A' && c<='Z'){f=1;}
+            if(c>='A' && c<='Z')cnt++;
+
         }
         if(cnt==word.length())return true;
         if(cnt==0)return true;
