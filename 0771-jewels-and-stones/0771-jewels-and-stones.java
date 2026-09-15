4d2829e70d6f11a43166f5401af2ee3a3fbe7d44
muzammil-360 <shaikhmuzammil5361@gmail.com>
2026-09-15 14:52:46 +0530

    Time: 2 ms (9.88%), Space: 43.3 MB (35.77%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        HashSet<Character> set=new HashSet<>();
+        for(char ch:jewels.toCharArray()){
+            set.add(ch);
+        }
+        int cnt=0;
+        for(char ch:stones.toCharArray()){
+            if(set.contains(ch))cnt++;
+        }
+        return cnt;
+    }
+}
