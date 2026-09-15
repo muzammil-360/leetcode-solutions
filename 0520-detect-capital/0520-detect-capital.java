0a82cc03679fe6aa242e638e8adf50413ccaf9e9
muzammil-360 <shaikhmuzammil5361@gmail.com>
2026-09-15 14:24:58 +0530

    Time: 1 ms (88.31%), Space: 43 MB (77.54%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        int cnt=0;
+        int f=0;
+        for(int ch=0;ch<word.length();ch++){
+            char c=word.charAt(ch);
+            int num=c-'0';
+             if(ch==0 && num<45){f=1;}
+            if(num<45)cnt++;
+        }
+        if(cnt==word.length())return true;
+        if(cnt==0)return true;
+        if(cnt==1 && f==1)return true;
+        return false;
+    }
+}
