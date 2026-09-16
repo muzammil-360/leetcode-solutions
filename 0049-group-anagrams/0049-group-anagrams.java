931aeb2e7c4f1dfecdf48dc365802446f89e0013
muzammil-360 <shaikhmuzammil5361@gmail.com>
2026-09-16 23:39:36 +0530

    Time: 20 ms (12.32%), Space: 51.1 MB (6.66%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -2,9 +2,14 @@ class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         HashMap<String,List<String>> map=new HashMap<>();
         for(String s:strs){
-            char[] arr=s.toCharArray();
-            Arrays.sort(arr);
-            String str=new String(arr);
+            // char[] arr=s.toCharArray();
+            // Arrays.sort(arr);
+            int[] freq=new int[26];
+            for(char ch:s.toCharArray()){
+                freq[ch-'a']++;
+            }
+           // String str=new String(freq);
+            String str=Arrays.toString(freq);
             if(!map.containsKey(str)){
                 map.put(str,new ArrayList<>());
             }
