c65d4128edf8515f0f077c327041ced341eb8ab0
muzammil-360 <shaikhmuzammil5361@gmail.com>
2026-09-20 16:57:54 +0530

    Time: 1 ms (99.94%), Space: 71.1 MB (72.08%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n=nums.length;
+        int i=0;
+        while(i<n){
+            int corrind=nums[i]-1;
+            if(nums[i]>0 && nums[i]<=n && nums[i]!=nums[corrind]){
+                int temp=nums[i];
+                nums[i]=nums[corrind];
+                nums[corrind]=temp;
+            }
+              else{i++;}
+        }
+        for(int j=0;j<n;j++){
+            if(nums[j]!=j+1){return j+1;}
+        }
+        return n+1;
+    }
+}
