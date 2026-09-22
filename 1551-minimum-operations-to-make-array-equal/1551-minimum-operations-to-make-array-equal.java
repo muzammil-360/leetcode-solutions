0f133df72bede9414936a1720afc338301f499ef
muzammil-360 <shaikhmuzammil5361@gmail.com>
2026-09-22 23:34:31 +0530

    Time: 0 ms (100%), Space: 42.2 MB (80.65%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.java
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.java
@@ -1,20 +1,23 @@
 class Solution {
     public int minOperations(int n) {
-        int[] arr=new int[n];
-        for(int i=0;i<n;i++){
-            arr[i]=2*i+1;
-        }
-        int sum=n;  //sum(n)/n-->n^2/n=n
-        int addsum=0;
-        int subsum=0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]<=sum){
-                addsum+=(sum-arr[i]);
-            }
-            else{
-                subsum+=(arr[i]-sum);
-            }
-        }
-        return Math.max(addsum,subsum);
+        // int[] arr=new int[n];
+        // for(int i=0;i<n;i++){
+        //     arr[i]=2*i+1;
+        // }
+        // int sum=n;  //sum(n)/n-->n^2/n=n
+        // int addsum=0;
+        // int subsum=0;
+        // for(int i=0;i<arr.length;i++){
+        //     if(arr[i]<=sum){
+        //         addsum+=(sum-arr[i]);
+        //     }
+        //     else{
+        //         subsum+=(arr[i]-sum);
+        //     }
+        // }
+        // return Math.max(addsum,subsum);
+        int mid=n/2;
+        if(n%2==0)return mid*mid;
+        return mid*(mid+1);
     }
 }
