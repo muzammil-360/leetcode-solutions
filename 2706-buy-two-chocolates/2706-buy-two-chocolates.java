2cb99ca9a99e4b583f2307a67148b0c6f0d38d7f
muzammil-360 <shaikhmuzammil5361@gmail.com>
2026-09-22 13:29:56 +0530

    Time: 108 ms (1.11%), Space: 45.6 MB (99.92%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.java
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.java
@@ -3,9 +3,23 @@ class Solution {
         if(prices.length==1){
             return money;
         }
-        Arrays.sort(prices);
-        int sum=prices[0]+prices[1];
+        int s=prices[0];
+        int ss=Integer.MAX_VALUE;
+        for(int i=1;i<prices.length;i++){
+            if(prices[i]<s){
+                ss=s;
+                s=prices[i];
+            }
+            else if(prices[i]>=s && prices[i]<ss){
+                ss=prices[i];
+            }
+        }
+        int sum=s+ss;
         if(sum<=money)return money-sum;
         return money;
+    //     Arrays.sort(prices);
+    //     int sum=prices[0]+prices[1];
+    //     if(sum<=money)return money-sum;
+    //     return money;
     }
 }
