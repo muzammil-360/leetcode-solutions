6e0fe19fec150150c5320ba8348b24a5f6a65a14
muzammil-360 <shaikhmuzammil5361@gmail.com>
2026-09-25 23:09:48 +0530

    Time: 8 ms (35.18%), Space: 47.9 MB (46.99%) - LeetHub

--- 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.java
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.java
@@ -1,12 +1,19 @@
 class Solution {
     public boolean doesAliceWin(String s) {
-        int vo_cnt=0;
-        for(int i=0;i<s.length();i++){
+        // int vo_cnt=0;
+        // for(int i=0;i<s.length();i++){
+        //     if(s.charAt(i)=='a' || s.charAt(i)=='e' || s.charAt(i)=='i' || s.charAt(i)=='o' || s.charAt(i)=='u'){
+        //         vo_cnt++;
+        //     }
+        // }
+        // if(vo_cnt==0)return false;
+        // return true;
+
+         for(int i=0;i<s.length();i++){
             if(s.charAt(i)=='a' || s.charAt(i)=='e' || s.charAt(i)=='i' || s.charAt(i)=='o' || s.charAt(i)=='u'){
-                vo_cnt++;
+                return true;
             }
         }
-        if(vo_cnt==0)return false;
-        return true;
+        return false;
     }
 }
